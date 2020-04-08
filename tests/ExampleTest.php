c2899e1635589de256f3fd786d6f5b920c32586d
Vitaliy <liseenko.vitaliy.vladimirovich@gmail.com>
2020-04-08 15:30:04 +0300

    Change phpunit command 10

---
+++ tests/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSum()
+    {
+        function sum($a, $b) {
+            return $a + $b;
+        }
+
+        $a = 2;
+        $b = 3;
+        $this->assertEquals(6, sum($a, $b));
+    }
+}
